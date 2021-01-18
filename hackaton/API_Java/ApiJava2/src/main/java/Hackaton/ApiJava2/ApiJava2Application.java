1e33eda04dd54c77f2cdb46e306aec741cc4bf7a
GabrielDEV <gabrielfernella07@hotmail.com>
2021-01-18 20:37:23 -0300

    Update ApiJava2Application.java

--- hackaton/API_Java/ApiJava2/src/main/java/Hackaton/ApiJava2/ApiJava2Application.java
+++ hackaton/API_Java/ApiJava2/src/main/java/Hackaton/ApiJava2/ApiJava2Application.java
@@ -11,3 +11,13 @@ public class ApiJava2Application {
 	}
 
 }
+
+/*
+* Processo de construção da API
+* 1 - Iniciar um projeto através do spring Initializing (O projeto deve possuir o mesmo java instalado, além dos pacotes que vc deseja ter ex: MySQL driver)
+* 2 - Crie seus controllers e seus models
+* 3 - Crie seu Repository para criar uma interface com os pacotes de CRUD, assim vc pode usar os métodos da biclioteca
+* 4 - na pasta sources e application.properties, add as configurações do banco de dados (MySQL)
+* 5 - Execute a aplicação e verifica se a rota está funcionando, ex: localhost:8080/api/status
+*
+* */
